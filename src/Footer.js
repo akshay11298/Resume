212e8c9e79558f6a3394d25908b8f4a676f5f82c
akshay11298 <akshay.avinash@gmail.com>
2018-04-20 17:29:45 +0530

    Prettier applied

--- src/Footer.js
+++ src/Footer.js
@@ -1,69 +1,121 @@
-import React, { Component } from 'react';
-import './Footer.css';
-import Icon from 'react-icons-kit';
-import { phone,mail4,plus } from 'react-icons-kit/icomoon';
+import React, { Component } from "react";
+import "./Footer.css";
+import {
+  FaPhone,
+  FaEnvelope,
+  FaPlusSquare,
+  FaFacebook,
+  FaGithubSquare,
+  FaLinkedin
+} from "react-icons/lib/fa";
 
 const phantomStyle = {
-    display: "block",
-    padding: "20px",
-    height: "60px",
-    width: "100%"
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%"
 };
 
 export class Footer extends Component {
-    render() {
-        return (
-            <div>
-                <div style={phantomStyle}/>
-            <div className="Footer">
-                <div className={"row"} style={{paddingLeft:'20%',paddingRight:'20%'}}>
-                    <div className={"col-md-4"}>
-                        <div className={"row section"}>
-                            <div className={"col-md-2 col-sm-2 heading"}>
-                                <div className={"icons"} >
-                                    <Icon icon={phone} size={'1.2em'}/>
-                                </div>
-                            </div>
-                            <div className={"col-md-10 col-sm-10 content"}>
-                                <strong>Call<br/>
-                                    +91-9971110289</strong>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"col-md-4"}>
-                        <div className={"row section"}>
-                            <div className={"col-md-2 col-sm-2 heading"}>
-                                <div className={"icons"}>
-                                    <Icon icon={mail4} size={'1.2em'}/>
-                                </div>
-                            </div>
-                            <div className={"col-md-10 col-sm-10 content"} >
-                                <strong>Contact<br/>
-                                    <a href={"mailto:akshay.avinash@gmail.com"} style={{color:'white'}}>akshay.avinash@gmail.com</a></strong>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"col-md-4"}>
-                        <div className={"row section"}>
-                            <div className={"col-md-2 col-sm-2 heading"}>
-                                <div className={"icons"}>
-                                    <Icon icon={plus} size={'1.2em'}/>
-                                </div>
-                            </div>
-                            <div className={"col-md-10 col-sm-10 content"}>
-                                <strong>Follow Me:<br/>
-                                    <div className={"row"}>
-                                        <a href="https://facebook.com/akshay11298" rel="noopener noreferrer" target="_blank"><i className="fa fa-facebook-official"></i></a>
-                                        <a href="https://github.com/akshay11298" rel="noopener noreferrer" target="_blank"><i className="fa fa-github-square"></i> </a>
-                                        <a href="https://www.linkedin.com/in/akshay-avinash-a4a941141/" rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin-square"></i></a>
-                                    </div>
-                                </strong>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div>
+        <div style={phantomStyle} />
+        <div className="Footer">
+          <div
+            className={"row"}
+            style={{ paddingLeft: "20%", paddingRight: "20%" }}
+          >
+            <div className={"col-md-4"}>
+              <div className={"row section"}>
+                <div className={"col-md-2 col-sm-2 heading"}>
+                  <div className={"icons"}>
+                    {/*<Icon icon={phone} size={'1.2em'}/>*/}
+                    <span style={{ fontSize: "1.75em" }}>
+                      <FaPhone />
+                    </span>
+                  </div>
                 </div>
+                <div className={"col-md-10 col-sm-10 content"}>
+                  <strong>
+                    Call<br />
+                    +91-9971110289
+                  </strong>
+                </div>
+              </div>
             </div>
+            <div className={"col-md-4"}>
+              <div className={"row section"}>
+                <div className={"col-md-2 col-sm-2 heading"}>
+                  <div className={"icons"}>
+                    {/*<Icon icon={mail4} size={'1.2em'}/>*/}
+                    <span style={{ fontSize: "1.75em" }}>
+                      <FaEnvelope />
+                    </span>
+                  </div>
+                </div>
+                <div className={"col-md-10 col-sm-10 content"}>
+                  <strong>
+                    Contact<br />
+                    <a
+                      href={"mailto:akshay.avinash@gmail.com"}
+                      style={{ color: "white" }}
+                    >
+                      akshay.avinash@gmail.com
+                    </a>
+                  </strong>
+                </div>
+              </div>
             </div>
-        );
-    }
+            <div className={"col-md-4"}>
+              <div className={"row section"}>
+                <div className={"col-md-2 col-sm-2 heading"}>
+                  <div className={"icons"}>
+                    {/*<Icon icon={plus} size={'1.2em'}/>*/}
+                    <span style={{ fontSize: "1.75em" }}>
+                      <FaPlusSquare />
+                    </span>
+                  </div>
+                </div>
+                <div className={"col-md-10 col-sm-10 content"}>
+                  <strong>
+                    Follow Me:<br />
+                    <div className={"row"}>
+                      <a
+                        href="https://facebook.com/akshay11298"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaFacebook style={{ color: "#3b5998" }} />
+                      </a>
+                      <a
+                        href="https://github.com/akshay11298"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaGithubSquare
+                          style={{
+                            color: "#303030",
+                            marginLeft: "5px",
+                            marginRight: "5px"
+                          }}
+                        />
+                      </a>
+                      <a
+                        href="https://www.linkedin.com/in/akshay-avinash-a4a941141/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaLinkedin style={{ color: "#337ab7" }} />
+                      </a>
+                    </div>
+                  </strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/Menu/Home/Home.js
+++ src/Menu/Home/Home.js
@@ -9,26 +9,26 @@ export class Home extends React.Component{
     };
     render(){
         return(
-            <div className={'row'} style={{marginTop:'2em'}}>
+            <div className={'row d-flex flex-solumn'} style={{marginTop:'2em'}}>
                 <div className={'col-md-6'}>
                     <img src={img} alt={"Me"} className={'img-fluid rounded-circle'}/>
                 </div>
                 <div className={'col-md-1'}/>
-                <div className={'col-md-5'}>
+                <div className={'col-md-5 home-content'}>
                     <div className={'font-tarsus'}>
-                        <strong><h1 style={{fontSize:'6em'}}>Hello,</h1></strong>
+                        <strong><h1 style={{fontSize:'5em'}}>Hello,</h1></strong>
                     </div>
                     <div className={'font-tarsus'}>
-                        <h2 style={{fontSize:'4em'}}>a bit about me:</h2>
+                        <h2 style={{fontSize:'3.5em'}}>a bit about me:</h2>
                     </div>
                     <div className={'row'}>
-                        <button type={'col-md-4 button'} className={'btn about-me font-tarsus'} style={{backgroundColor:'#E83556'}} onClick={()=>this.handleContentChange(1)}>
+                        <button type={'button'} className={'btn about-me font-tarsus'} style={{backgroundColor:'#E83556'}} onClick={()=>this.handleContentChange(1)}>
                             <h5>Resume</h5>
                         </button>
-                        <button type={'col-md-4 button'} className={'btn about-me font-tarsus'} style={{backgroundColor:'#61d0d4'}} onClick={()=>this.handleContentChange(2)}>
+                        <button type={'button'} className={'btn about-me font-tarsus'} style={{backgroundColor:'#61d0d4'}} onClick={()=>this.handleContentChange(2)}>
                             <h5>Projects</h5>
                         </button>
-                        <button type={'col-md-4 button'} className={'btn about-me font-tarsus'} style={{backgroundColor:'#D1C926'}} onClick={()=>this.handleContentChange(3)}>
+                        <button type={'button'} className={'btn about-me font-tarsus'} style={{backgroundColor:'#D1C926'}} onClick={()=>this.handleContentChange(3)}>
                             <h5>Contact Me</h5>
                         </button>
                     </div>

91baca05a83e60f3df52ad0c2f22d185a33113f4
Akshay Avinash <akshay.avinash@sap.com>
2024-11-17 20:07:55 +0530

    Extract work experience component

---
+++ src/Menu/Resume/WorkExperience.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export const workExperience = [{
+    title: "React Native Intern",
+    period: {
+        start: "11 December 2017",
+        end: "22 January 2018"
+    },
+    organisation: "Dipper Technologies",
+    content: <div>
+        I interned as a React Native developer in the organisation. I
+        worked on building cross platform applications using React
+        Native. Since the technology was new to me, it provided a great
+        working and learning experience. I worked in a team, and was
+        consistently guided by my supervisor to overcome my
+        shortcomings. We had two tasks at hand:
+        <ul>
+            <li>
+                {' '}
+                Import one of the existing apps from Ionic Framework to React
+                Native
+            </li>
+            <li>
+                {' '}
+                Develop and Implement a new EPOD (Electronic Proof of Delivery) application
+            </li>
+        </ul></div>
+}, {
+    title: "Software Developer Intern",
+    period: {
+        start: "14 June 2017",
+        end: "10 July 2017"
+    },
+    organisation: "NBCC (India) Ltd",
+    content: <div>
+        I interned as a Software developer in the organisation. I worked
+        on building a desktop application for the organisation for the
+        purpose of canteen management. I planned and developed the
+        complete application on my own with some feedback from my
+        colleagues and help from my seniors. I developed the application
+        using JavaFx. The main idea was to:
+        <ul>
+            <li> Reduce labour load and paper wastage</li>
+            <li> Efficient and easy to use interface</li>
+        </ul></div>
+}
+]
